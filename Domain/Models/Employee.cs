d7bd0c240429219ecab0fc090ce321bca2b3b15f
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 07:58:02 +0400

    added notification list in employee model

--- Domain/Models/Employee.cs
+++ Domain/Models/Employee.cs
@@ -10,6 +10,10 @@ public class Employee
     public string Position { get; private set; }
     public string Department { get; private set; }
 
+    private readonly List<Notification> _notifications = [];
+
+    public IReadOnlyCollection<Notification> Notifications => _notifications.AsReadOnly();
+
     #pragma warning disable CS8618 // Required for ef core
     private Employee() { }
     #pragma warning restore CS8618 
@@ -29,5 +33,12 @@ public class Employee
         LastName = lastName;
         Position = position;
         Department = department;
+    }
+
+    public void AddNotification(string title, string text)
+    {
+        var notification = new Notification(title, text);
+
+        _notifications.Add(notification);
     }
 }
