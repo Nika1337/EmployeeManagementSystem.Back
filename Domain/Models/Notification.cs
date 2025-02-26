0f455f0a9999671f7ca12368a5ff9c8271a290aa
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 07:53:41 +0400

    added notification model

---
+++ Domain/Models/Notification.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Domain.Models;
+
+public class Notification
+{
+    public Guid Id { get; private init; }
+    public string Title { get; private init; }
+    public string Text { get; private init; }
+    public DateTime CreatedAtUtc { get; private init; }
+    public bool IsRead { get; private set; }
+
+    #pragma warning disable CS8618 // needed for ef core
+    private Notification() { }
+    #pragma warning restore CS8618
+
+    public Notification(string title, string text)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+        Text = text;
+        CreatedAtUtc = DateTime.UtcNow;
+        IsRead = false;
+    }
+
+    public void MarkAsRead()
+    {
+        IsRead = true;
+    }
+}
