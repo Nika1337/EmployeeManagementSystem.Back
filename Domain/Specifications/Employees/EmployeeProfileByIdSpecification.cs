3171adf236dcad52822cf96c9d0bf16e9f5ec1cf
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 10:57:10 +0400

    added specification for employee profile

---
+++ Domain/Specifications/Employees/EmployeeProfileByIdSpecification.cs
@@ -0,0 +1,24 @@
+﻿
+using Ardalis.Specification;
+using Domain.Models;
+using Domain.Specifications.Employees.Results;
+
+namespace Domain.Specifications.Employees;
+
+public class EmployeeProfileByIdSpecification : SingleResultSpecification<Employee, EmployeeProfileResult>
+{
+    public EmployeeProfileByIdSpecification(Guid id)
+    {
+        Query.Where(empl => empl.Id == id);
+
+        Query.Select(empl => new EmployeeProfileResult
+        {
+            FirstName = empl.FirstName,
+            LastName = empl.LastName,
+            Position = empl.Position,
+            Department = empl.Department
+        });
+
+        Query.AsNoTracking();
+    }
+}
