d3297c069d9660b3234ef645e44058898ac05764
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 09:11:18 +0400

    added specification for getting notifications by employee id

---
+++ Domain/Specifications/EmployeeNotifications/NotificationsByEmployeeIdSpecification.cs
@@ -0,0 +1,25 @@
+﻿using Ardalis.Specification;
+using Domain.Models;
+using Domain.Specifications.EmployeeNotifications.Results;
+
+
+namespace Domain.Specifications.EmployeeNotifications;
+
+public class NotificationsByEmployeeIdSpecification : Specification<EmployeeNotification, NotificationResult>
+{
+    public NotificationsByEmployeeIdSpecification(Guid employeeId)
+    {
+        Query.Where(en => en.Employee.Id == employeeId);
+
+        Query.Select(en => new NotificationResult
+        {
+            Id = en.Notification.Id,
+            Title = en.Notification.Title,
+            Text = en.Notification.Text,
+            CreatedAt = en.Notification.CreatedAtUtc,
+            IsRead = en.IsRead,
+        });
+
+        Query.AsNoTracking();
+    }
+}
