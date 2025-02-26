93d6837676a12dfdb5f32e08d82d2e9cd338e0ba
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 17:30:48 +0400

    added configuration for employee notification

---
+++ Infrastructure/Data/Config/EmployeeNotificationConfiguration.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config;
+
+internal class EmployeeNotificationConfiguration : IEntityTypeConfiguration<EmployeeNotification>
+{
+    public void Configure(EntityTypeBuilder<EmployeeNotification> builder)
+    {
+        builder.HasKey("EmployeeId", "NotificationId");
+
+        builder.HasOne(en => en.Employee)
+            .WithMany(e => e.Notifications)
+            .HasForeignKey("EmployeeId")
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(en => en.Notification)
+            .WithMany(n => n.Recipients)
+            .HasForeignKey("NotificationId")
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
