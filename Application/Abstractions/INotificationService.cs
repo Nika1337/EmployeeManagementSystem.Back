9f0860ac7cc1009a67be3dac7565874d4287278b
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 17:03:02 +0400

    added service interface for notifications

---
+++ Application/Abstractions/INotificationService.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Application.DataTransferObjects.Notifications;
+
+namespace Application.Abstractions;
+
+public interface INotificationService
+{
+    Task<IEnumerable<NotificationResponse>> GetNotificationsOfEmployeeAsync(Guid employeeId);
+    Task MarkAsReadAsync(Guid employeeId, Guid notificationId);
+}
