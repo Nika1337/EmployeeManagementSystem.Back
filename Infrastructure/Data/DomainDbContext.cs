8c75a0634b390aa5f664babd1ccd93d1157dfe0f
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 17:32:29 +0400

    added db context for domain models

---
+++ Infrastructure/Data/DomainDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Models;
+using Infrastructure.Data.Config;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infrastructure.Data;
+
+internal class DomainDbContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+    public DbSet<EmployeeNotification> EmployeeNotifications { get; set; }
+
+    public DomainDbContext(DbContextOptions<DomainDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new EmployeeConfiguration());
+        builder.ApplyConfiguration(new NotificationConfiguration());
+        builder.ApplyConfiguration(new EmployeeNotificationConfiguration());
+    }
+}
