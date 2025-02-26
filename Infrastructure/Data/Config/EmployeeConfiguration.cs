2f067f81ef5c81c70328a5530df37557ad1efd76
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 17:27:03 +0400

    added configuration for employee

---
+++ Infrastructure/Data/Config/EmployeeConfiguration.cs
@@ -0,0 +1,33 @@
+﻿
+
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config;
+
+internal class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+{
+    public void Configure(EntityTypeBuilder<Employee> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.FirstName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(e => e.LastName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(e => e.Position)
+            .HasMaxLength(100);
+
+        builder.Property(e => e.Department)
+            .HasMaxLength(100);
+
+        builder.HasMany(e => e.Notifications)
+            .WithOne(en => en.Employee)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
