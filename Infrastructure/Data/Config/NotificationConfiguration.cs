72a02c1c2ec50a5de46644385d5460467a79d044
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 17:27:37 +0400

    added configuration for notification

---
+++ Infrastructure/Data/Config/NotificationConfiguration.cs
@@ -0,0 +1,30 @@
+﻿
+
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config;
+
+internal class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+{
+    public void Configure(EntityTypeBuilder<Notification> builder)
+    {
+        builder.HasKey(n => n.Id);
+
+        builder.Property(n => n.Title)
+            .IsRequired()
+            .HasMaxLength(255);
+
+        builder.Property(n => n.Text)
+            .IsRequired()
+            .HasMaxLength(1000);
+
+        builder.Property(n => n.CreatedAtUtc)
+            .IsRequired();
+
+        builder.HasMany(n => n.Recipients)
+            .WithOne(en => en.Notification)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
