0018674b088f1f6fb75263053d7bd2c9ded8fcd0
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 17:40:37 +0400

    added context for identity

---
+++ Infrastructure/Identity/IdentityContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Identity;
+
+internal class IdentityContext(DbContextOptions<IdentityContext> options) : IdentityDbContext(options)
+{
+
+}
