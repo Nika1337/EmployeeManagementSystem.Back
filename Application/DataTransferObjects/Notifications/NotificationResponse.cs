052a2a3d9529ff0951da7b7dd465c6ebd5cd691b
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 16:42:04 +0400

    added notification response

---
+++ Application/DataTransferObjects/Notifications/NotificationResponse.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Application.DataTransferObjects.Notifications;
+
+public record NotificationResponse
+{
+    public required Guid Id { get; init; }
+    public required string Title { get; init; }
+    public required string Text { get; init; }
+    public required DateTime CreatedAt { get; init; }
+    public required bool IsRead { get; init; }
+}
