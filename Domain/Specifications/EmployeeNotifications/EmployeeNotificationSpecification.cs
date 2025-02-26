5f167ea60890aa86cfb726b2a71a25d78a80f6bf
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 09:11:42 +0400

    added specification for getting EmployeeNotification specification object for changing

---
+++ Domain/Specifications/EmployeeNotifications/EmployeeNotificationSpecification.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Ardalis.Specification;
+using Domain.Models;
+
+namespace Domain.Specifications.EmployeeNotifications;
+
+public class EmployeeNotificationSpecification : SingleResultSpecification<EmployeeNotification>
+{
+    public EmployeeNotificationSpecification(Guid employeeId, Guid notificationId)
+    {
+        Query.Where(en => en.Employee.Id == employeeId && en.Notification.Id == notificationId);
+
+        Query.AsTracking();
+    }
+}
