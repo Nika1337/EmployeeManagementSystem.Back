db164206cd1d516c60774154271f25963a28619f
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 15:38:33 +0400

    added password incorrect exception

---
+++ Domain/Exceptions/PasswordIncorrectException.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Domain.Exceptions;
+
+public class PasswordIncorrectException : Exception
+{
+    public PasswordIncorrectException() : base($"Password is incorrect")
+    {
+
+    }
+}
